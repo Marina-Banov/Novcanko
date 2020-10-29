ca618da63f8752519a12a3c7218e97ae438c3cfd
mbanov <m.banov7@gmail.com>
2020-10-29 20:54:56 +0100

    added start menu

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Hosting;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void GoToShop()
+    {
+
+    }
+
+    public void GoToWallet()
+    {
+
+    }
+
+    public void EditLevels()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
