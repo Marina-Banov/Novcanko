026fa2e6a006bc623d8aed8ba024f929c9031bcc
mbanov <m.banov7@gmail.com>
2021-01-01 16:32:16 +0100

    AddToWallet scene

---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData : MonoBehaviour
+{
+    [SerializeField] private Wallet wallet = new Wallet();
+
+    public void SaveToJson()
+    {
+        System.IO.File.WriteAllText(Application.persistentDataPath + "/wallet.json", JsonUtility.ToJson(wallet));
+    }
+}
+
+[System.Serializable]
+public class Wallet
+{
+    public List<WalletItem> walletItems = new List<WalletItem>();
+}
+
+[System.Serializable]
+public class WalletItem
+{
+    public string name;
+    public int value;
+    public int quantity;
+}
