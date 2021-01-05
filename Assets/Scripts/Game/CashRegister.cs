5e128216acb942ef08911908f26e929f730c4d56
Lena Novak <lnovak@croz.net>
2021-01-05 23:38:12 +0100

    adding game prototype to the application

---
+++ Assets/Scripts/Game/CashRegister.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CashRegister : MonoBehaviour, IDropHandler {
+    public void OnDrop(PointerEventData eventData) {
+		if (eventData.pointerDrag != null && !eventData.pointerDrag.GetComponent<DragDrop>().droppedOnSlot) {
+			GiveAmount.givenNumber += DragDrop.moneyNumber;
+			eventData.pointerDrag.GetComponent<DragDrop>().droppedOnSlot = true;
+			eventData.pointerDrag.GetComponent<RectTransform>().gameObject.tag = "cashReg";
+		}
+	} 
+}
