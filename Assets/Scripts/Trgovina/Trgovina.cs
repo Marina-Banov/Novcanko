7db04a604fc7034426913a76d411a2c3172e10bd
Ostix626 <martinluk.luk@gmail.com>
2021-03-06 05:56:54 +0100

    custom number pad

--- Assets/Scripts/Trgovina/Trgovina.cs
+++ Assets/Scripts/Trgovina/Trgovina.cs
@@ -15,21 +15,24 @@ public class Trgovina : MonoBehaviour
     static public float amount;
     static public Boolean krupno;
     public GameObject inputField;
+    public TextMeshProUGUI input_price;
+    private int lipe;
 
     void Start()
     {
+        lipe = -1;
         krupno = false;
         string jsonString = System.IO.File.ReadAllText(StartMenu.walletSavePath);
         wallet = JsonUtility.FromJson<Wallet>(jsonString);
         COINS_ITEMS = wallet.coins.Count;
         BANKNOTES_ITEMS = wallet.banknotes.Count;
-        total.text = "U novčaniku imam:\n " + GetTotalValue().ToString() + " kn";
+        total.text = String.Format("U novčaniku:\n{0:0.00} kn", GetTotalValue());
     }
 
     void Update()
     {
-        string input = inputField.GetComponent<TMP_InputField>().text;
-        if (input.Length > 0) amount = float.Parse(input);
+        string input_txt = inputField.GetComponent<TMP_InputField>().text;
+        if (input_txt.Length > 0) amount = float.Parse(input_txt, System.Globalization.CultureInfo.GetCultureInfo("hr-HR").NumberFormat);
         else amount = 0;
     }
 
@@ -51,5 +54,32 @@ public class Trgovina : MonoBehaviour
     {
         krupno = true;
         SceneManager.LoadScene("Placanje");
+    }
+
+    public void WriteNum(int n)
+    {
+        TMP_InputField input = inputField.GetComponent<TMP_InputField>();
+        input.interactable = true;
+
+        if (n == -1)
+        {
+            if (input.text.Length > 0) input.text = input.text.Remove(input.text.Length - 1, 1);
+            if (lipe >= 0) lipe += 1;
+            if (lipe == 3) lipe = -1;
+        }
+
+        else if (n == -2 && lipe < 0)
+        {
+            if (input.text.Length == 0) input.text = "0";
+            input.text += ",";
+            lipe = 2;
+        }
+
+        else if (n >= 0 && lipe != 0)
+        {
+            input.text += n.ToString();
+            if (lipe > 0) lipe -= 1;
+        }
+        if (input.text == "0") input.text = input.text.Remove(input.text.Length - 1, 1);
     }
 }
