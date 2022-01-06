cf327c34cbf38c5dd0809a015803e7345bbd35fc
lenanovak <novak.lena98@gmail.com>
2022-01-07 00:49:26 +0100

    adds blur background, button animation, hides repeating text

--- Assets/Scripts/Game/ShowExitScreen.cs
+++ Assets/Scripts/Game/ShowExitScreen.cs
@@ -8,15 +8,15 @@ public class ShowExitScreen : MonoBehaviour
 {
     public Canvas canvas;
     public GameObject panel;
-    //public GameObject lastLevel = 0;
+    public GameObject blur;
     public TextMeshProUGUI lastLevel, showLevel, highscore;
-    //public GameObject showLevel = 0;
 
     public void showPanel()
     {
         if (panel.activeSelf == true)
         {
             panel.gameObject.SetActive(false);
+            blur.gameObject.SetActive(false);
         } else
         {
             if (System.Convert.ToInt32(PlayerPrefs.GetString("highscore")) < System.Convert.ToInt32(lastLevel.text))
@@ -27,6 +27,7 @@ public class ShowExitScreen : MonoBehaviour
             showLevel.text = (System.Convert.ToInt32(lastLevel.text) - 1).ToString();
             highscore.text = "TVOJ OSOBNI REKORD: " + PlayerPrefs.GetString("highscore");
             panel.gameObject.SetActive(true);
+            blur.gameObject.SetActive(true);
         }
     }
 }
